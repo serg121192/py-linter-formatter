a0ae5390fcdd47176c34c3ccab1938454b4e83e1
srx <srx92@protonmail.com>
2024-10-07 17:09:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,52 @@
+# from Tools.scripts.fixdiv import report
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [
+            {
+                "line": error["line_number"],
+                "column": error["column_number"],
+                "message": error["text"],
+                "name": error["code"],
+                "source": "flake8"
+            }
+            for error in errors
+        ],
+        "path": file_path,
+        "status": "failed"
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        {
+            "errors": [],
+            "path": report,
+            "status": "passed"
+        } if linter_report[report] == [] else {
+            "errors": [
+                {
+                    "line": error["line_number"],
+                    "column": error["column_number"],
+                    "message": error["text"],
+                    "name": error["code"],
+                    "source": "flake8"
+                }
+                for error in linter_report[report]
+            ],
+            "path": report,
+            "status": "failed"
+        }
+        for report in linter_report
+    ]

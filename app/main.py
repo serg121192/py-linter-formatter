5287941da148e1031808548217d5c1eb2ea2e374
srx <srx92@protonmail.com>
2024-10-08 15:37:56 +0300

    Errors call fix

--- app/main.py
+++ app/main.py
@@ -10,10 +10,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors" : [] if not errors else
-        [
-            format_linter_error(error) for error in errors
-        ],
+        "errors" : [format_linter_error(error) for error in errors],
         "path" : file_path,
         "status" : "failed" if errors else "passed"
     }

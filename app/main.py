be2b63e674e18d1adcf0067c7110cb849df8c7f5
srx <srx92@protonmail.com>
2024-10-08 11:02:51 +0300

    Updated solution

--- app/main.py
+++ app/main.py
@@ -1,52 +1,30 @@
-# from Tools.scripts.fixdiv import report
-
-
 def format_linter_error(error: dict) -> dict:
     return {
-        "line": error["line_number"],
-        "column": error["column_number"],
-        "message": error["text"],
-        "name": error["code"],
-        "source": "flake8"
+        "line" : error["line_number"],
+        "column" : error["column_number"],
+        "message" : error["text"],
+        "name" : error["code"],
+        "source" : "flake8"
     }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
-        "errors": [
-            {
-                "line": error["line_number"],
-                "column": error["column_number"],
-                "message": error["text"],
-                "name": error["code"],
-                "source": "flake8"
-            }
+        "errors" : [],
+        "path" : file_path,
+        "status" : "passed"
+    } if errors == [] else {
+        "errors" : [
+            format_linter_error(error)
             for error in errors
         ],
-        "path": file_path,
-        "status": "failed"
+        "path" : file_path,
+        "status" : "failed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        {
-            "errors": [],
-            "path": report,
-            "status": "passed"
-        } if linter_report[report] == [] else {
-            "errors": [
-                {
-                    "line": error["line_number"],
-                    "column": error["column_number"],
-                    "message": error["text"],
-                    "name": error["code"],
-                    "source": "flake8"
-                }
-                for error in linter_report[report]
-            ],
-            "path": report,
-            "status": "failed"
-        }
-        for report in linter_report
+        format_single_linter_file(file_path, errors)
+        for file_path, errors in linter_report.items()
     ]
